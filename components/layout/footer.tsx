03826c60e50e636ff3863b16e56ffaef6ab53a6c
Jörg Schäffer <joerg.schaeffer@hygraph.com>
2025-08-25 17:16:20 +0200

    naming consistency

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -146,7 +146,7 @@ export default async function Footer() {
 
           <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
             <p>
-              © {new Date().getFullYear()} Coffee Roaster. All rights reserved.
+              © {new Date().getFullYear()} Third Wave Coffee Roaster. All rights reserved.
             </p>
           </div>
         </div>
