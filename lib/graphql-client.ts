7ab744d9fd0437f102a9d1b1505f5f1140670a7a
Daniil Tykheev <daniiltykheev@gmail.com>
2025-10-01 16:22:27 +0200

    Fix CMS caching: Set revalidation to 0 for immediate content updates

--- lib/graphql-client.ts
+++ lib/graphql-client.ts
@@ -16,7 +16,7 @@ export async function fetchFromCMS(query: string, variables = {}) {
         query,
         variables,
       }),
-      next: { revalidate: 60 }, // Revalidate every minute
+      next: { revalidate: 0 }, // No caching - fetch fresh data every time
     });
 
     const json = await res.json();
